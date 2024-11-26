c6c6876f0a6350b9cbc1bdddb31adae1e0321a4f
Sahar <sahar.nasiminezhad@uantwerpen.be>
2024-11-26 11:56:15 +0100

    run scripts updated

--- run_scripts.ps1
+++ run_scripts.ps1
@@ -1,6 +1,7 @@
-Start-Process -NoNewWindow -FilePath "python" -ArgumentList "conveyor_belt_G2MQTT/GtoMQTT.py"
-Start-Process -NoNewWindow -FilePath "python" -ArgumentList "crane_optimal_control/mqtt_database_writer"
-Start-Process -NoNewWindow -FilePath "python" -ArgumentList "crane_optimal_control/mqtt_gantry_controller.py"
-Start-Process -NoNewWindow -FilePath "python" -ArgumentList "crane_optimal_control/mqtt_trajectory_generator.py"
+Start-Process  -FilePath "python" -ArgumentList "conveyor_belt_G2MQTT/GtoMQTT.py"
+Start-Process  -FilePath "python" -ArgumentList "crane_optimal_control/mqtt_database_writer"
+Start-Process  -FilePath "python" -ArgumentList "crane_optimal_control/mqtt_gantry_controller.py"
+Start-Process  -FilePath "python" -ArgumentList "crane_optimal_control/mqtt_trajectory_generator.py"
+Start-Process  -FilePath "python" -ArgumentList "ship_simulator/src/shipsimulationmain.py"
 
 Write-Host "All scripts have been started."
